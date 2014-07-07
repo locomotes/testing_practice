0f48bdf8171437e23c28022347c027cb5ef8a197
Jonathan Dickerson <jongdickerson@gmail.com>
2014-07-07 17:39:43 -0400

    completed 5 tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,49 @@
 /* global describe, it */
 
+
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    // describe('cars', function(){
+	   //  it('should return a length of 1', function () {
+	   //    expect('cars').to.have.length(4);
+	   //  });
+    // 	it('should be a an array', function() {
+    // 		expect('cars').to.be.a('array');
+    // 	});
+    // });
 
-            });
-        });
-    });
+    // describe('stooges', function () {
+    // 	it('should be a string')
+    // })
+		describe('lotto', function () {
+			it('should have a number between 100 and 120', function () {
+				expect(lotto).to.be.within(100, 120);
+			});
+		});
+
+		describe('earth', function () {
+			it('should not contain forest or ocean', function () {
+				expect(earth).to.not.have.members(['forest', 'ocean'])
+			});
+		});
+
+
+		describe('decimals', function () {
+			it('should be close to 1', function () {
+				expect(decimals).to.be.closeTo(.5, 1.5);
+			});
+		});
+
+		describe('bounty_hunter', function () {
+			it('should not exist', function () {
+				expect(bounty_hunter).to.not.exist;
+			})
+		})
+
+		describe('prince.albums', function () {
+			it('should contain purple rain', function () {
+				expect(prince.albums).to.have.string('purple rain');
+			});
+		});
 })();
